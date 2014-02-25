040322600059f212094b95492b0c3ede767e08bb
Goodzila <rrinat1992@gmail.com>
2014-02-26 03:51:10 +0600

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,60 @@
+LOCAL_PATH := device/meizu/mx2
+
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/meizu/mx2/BoardConfigVendor.mk
+
+TARGET_PREBUILT_KERNEL := device/meizu/mx2/kernel
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+EXYNOS4X12_ENHANCEMENTS := true
+EXYNOS4_ENHANCEMENTS := true
+
+ifdef EXYNOS4X12_ENHANCEMENTS
+COMMON_GLOBAL_CFLAGS += -DEXYNOS4_ENHANCEMENTS
+COMMON_GLOBAL_CFLAGS += -DEXYNOS4X12_ENHANCEMENTS
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+endif
+
+TARGET_BOARD_PLATFORM := exynos4
+TARGET_SOC := exynos4x12
+TARGET_BOOTLOADER_BOARD_NAME := mx2
+
+# Kernel
+BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200"
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := device/meizu/mx2/kernel
+
+# Recovery
+BOARD_CUSTOM_BOOTIMG_MK := device/meizu/mx2/customrecovery.mk
+TARGET_RECOVERY_INITRC := device/meizu/mx2/recovery/recovery.rc
+TARGET_PROVIDES_INIT := true
+TARGET_PROVIDES_INIT_TARGET_RC := true
+BOARD_CUSTOM_GRAPHICS := ../../../device/meizu/mx2/recovery/graphics.c
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
+
+BOARD_FLASH_BLOCK_SIZE := 4096
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 815792128
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2198863872
+BOARD_CACHEIMAGE_PARTITION_SIZE := 214748364
+
---
+++ README.md
@@ -0,0 +1,13 @@
+Recovery for Meizu MX2
+========================
+This is source for compile CWM Recovery for Meizu MX2
+
+Build Recovery
+--------------------------------
+- cd ~/androidsource/
+- . build/envsetup.sh
+- lunch
+- select device
+- make recoveryimage
+
+Your Recovery - /androidsource/out/target/product/mx2/recovery.img
